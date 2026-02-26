809b98dccd510338be60f757942db18fce719f4b
Jitin Kumar <jk9120810343@gmail.com>
2026-02-27 00:32:38 +0530

    Create  variables.js

---
+++ JS Basics/variables.js
@@ -0,0 +1,14 @@
+ let age = 20;
+ let score = 90;
+ score = 95;   // allowed
+
+const pi = 3.14;
+ pi = 3.1415;  // Error
+
+const arr = [1, 2];
+arr.push(3);   // allowed
+console.log(arr);
+
+
+var x = 10;// not used currently
+console.log(x);
